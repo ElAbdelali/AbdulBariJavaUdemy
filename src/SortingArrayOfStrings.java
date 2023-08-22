57d10e386ab24221edab3e538eb40588bfd11606
ElAbdelali <eljaouhariabdelali@outlook.com>
2023-08-21 23:17:52 -0500

    Using Array's built in method, sort, to sort through an array of strings.

---
+++ src/SortingArrayOfStrings.java
@@ -0,0 +1,12 @@
+public class SortingArrayOfStrings {
+    public static void main(String[] args) {
+        // Sorting an array of strings
+
+        String arrayyy[]={"java","python","pascal","smalltalk","ada","basic"};
+
+        java.util.Arrays.sort(arrayyy);
+
+        for(String x:arrayyy)
+            System.out.println(x);
+    }
+}
