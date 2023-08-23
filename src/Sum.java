a53f64747601ccabe2cd082469f0f152d89fae6f
ElAbdelali <eljaouhariabdelali@outlook.com>
2023-08-22 22:41:00 -0500

    Uitilized command line arguments. Program parses the string into double values and outputs the sum of the parsed values.

---
+++ src/Sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+    public static void main(String[] args) {
+        double sum = 0;
+        for (String s: args){
+            sum += Double.parseDouble(s);
+        }
+        System.out.println(sum);
+    }
+
+}
